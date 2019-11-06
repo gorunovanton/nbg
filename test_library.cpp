c2c316e82f4949dc40626efdea06ca3b2ad12d1a
AntonGorunov <gorunov.anton@gmail.com>
2019-11-06 22:46:33 +0300

    - added C++ test library

---
+++ test_library.cpp
@@ -0,0 +1,7 @@
+#include "test_library.h"
+
+#include <iostream>
+
+void hello() { std::cout << "Hello, World!" << std::endl; }
+
+int get_five() { return 5; }
---
+++ test_library.h
@@ -0,0 +1,7 @@
+#ifndef NBG_LIBRARY_H
+#define NBG_LIBRARY_H
+
+void hello();
+int get_five();
+
+#endif // NBG_LIBRARY_H
